6a7167d7e8fb7327dca3b84755b440e8ad8685e1
syunsuke <2002syunsuke@gmail.com>
2025-11-12 10:30:04 +0900

    fix

---
+++ modules/alb/variables.tf
@@ -0,0 +1,21 @@
+variable "alb_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = string
+}
+
+variable "instance_id" {
+  type = string
+}
+
+variable "security_group_alb_ids" {
+  type = list(string)
+}
+
+
