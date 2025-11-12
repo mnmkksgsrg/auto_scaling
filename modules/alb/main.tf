bf5b194fd6b62d810d11e9e327e16a6b1f65b7f8
syunsuke <2002syunsuke@gmail.com>
2025-11-12 10:25:58 +0900

    fix

---
+++ modules/alb/main.tf
@@ -0,0 +1,32 @@
+resource "aws_lb" "alb" {
+  internal = "false"
+  load_balancer_type = "application"
+  security_grooups = var.security_group_alb_ids
+  subnets = var.public_subnets_ids
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_target_group" "instance" {
+  port = 80
+  protocol = "HTTP"
+  vpc_id = var.vpc_id
+  target_type = "instance"
+}
+
+resource "aws_lb_target_group_attachment" "instance" {
+  target_group_arn = aws_lb_target_group.instance.arn
+  target_id = var.instance_id
+  port = 80
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb_alb.arn
+  port = 80
+  prptocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.instance.arn
+  }
+}
+
